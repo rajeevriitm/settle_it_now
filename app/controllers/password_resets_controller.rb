55d94ac5191abd910c760b693b8cc11b19515e9c
rajeev <rajeevriitm@gmail.com>
2015-06-30 20:45:59 +0530

    add password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,57 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user,only: [:edit,:update]
+  before_action :valid_user,only: [:edit,:update]
+  before_action :check_expiration_date,only: [:edit,:update]
+
+  def new
+  end
+
+  def create
+    if @user=User.find_by(email: params[:password_reset][:email].downcase)
+      @user.create_reset_digest
+      @user.send_reset_email
+      flash[:info]="Reset email sent"
+      redirect_to root_url
+    else
+      flash.now[:danger]="User doesnt exist"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+  def update
+    if password_blank?
+      flash.now[:danger]="password cant be blank"
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      login @user
+      flash[:success]="Password resdet"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def get_user
+    @user=User.find_by(email: params[:email])
+  end
+  def valid_user
+    unless @user && @user.activated? && @user.authenticated?(:reset,params[:id])
+      redirect_to root_url
+    end
+  end
+  def password_blank?
+    params[:user][:password].blank?
+  end
+  def user_params
+    params.require(:user).permit(:password,:password_confirmation)
+  end
+  def check_expiration_date
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token,:activation_token
+  attr_accessor :remember_token,:activation_token,:reset_token
   before_create :create_activation_digest
   before_save {email.downcase!}
   validates :name,presence: true,length: {maximum: 50}
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
 #remembers a user remember digest in database
   def remember
     self.remember_token=User.new_token
-    update_attribute(:remember_digest,User.digest(remember_token))
+    self.update_attribute(:remember_digest,User.digest(remember_token))
   end
 
   #authenticate the remember digest with token
@@ -43,7 +43,17 @@ class User < ActiveRecord::Base
       self.update_attribute(:activated,true)
       self.update_attribute(:activated_at,Time.zone.now)
   end
-
+  def create_reset_digest
+    self.reset_token=User.new_token
+    update_attribute(:reset_digest,User.digest(reset_token))
+    update_attribute(:reset_sent_time,Time.zone.now)
+  end
+  def send_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  def password_reset_expired?
+    reset_sent_time < 2.hours.ago
+  end
 
   private
 
