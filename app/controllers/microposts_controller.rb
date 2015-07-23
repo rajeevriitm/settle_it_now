2bf535e3ed96e83ab41a6f98fa431532930eb67e
rajeev <rajeevriitm@gmail.com>
2015-07-23 08:08:09 +0530

    editss

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -45,7 +45,7 @@ class MicropostsController < ApplicationController
     redirect_to request.referrer || root_url
   end
   def more
-    @micropost=Micropost.find_by(params[:id])
+    @micropost=Micropost.find(params[:id])
     @answers=@micropost.answers
     # redirect_to root_url
     respond_to do |format|
