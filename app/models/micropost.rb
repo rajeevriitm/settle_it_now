2e904c3409f7c4ab39bbb10801a53124d91d0a29
rajeev <rajeevriitm@gmail.com>
2015-07-26 05:22:28 +0530

    fetch-feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,22 +1,22 @@
 class Micropost < ActiveRecord::Base
-  after_save :micropost_activity
+  # after_save :micropost_activity
   belongs_to :user
   has_many :answers,dependent: :destroy
-  has_many :activities,as: :action,dependent: :destroy
-  # default_scope -> { order(created_at: :desc) }
+  # has_many :activities,as: :action,dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
   validates :user_id,presence:true
   validates :content, presence: true
   mount_uploader :picture,PictureUploader
   validate :picture_size
 
-  ActiveRecord::Base.transaction do
-    def micropost_activity
-      activities.create!(owner_id: user_id,user_id: user_id,micropost_id: id)
-      user.followers.each do |follower|
-        activities.create!(owner_id: user_id,user_id: follower.id,micropost_id: id)
-      end
-    end
-  end
+  # ActiveRecord::Base.transaction do
+  #   def micropost_activity
+  #     activities.create!(owner_id: user_id,user_id: user_id,micropost_id: id)
+  #     user.followers.each do |follower|
+  #       activities.create!(owner_id: user_id,user_id: follower.id,micropost_id: id)
+  #     end
+  #   end
+  # end
 
   private
   def picture_size
