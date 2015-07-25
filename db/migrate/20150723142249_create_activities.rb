5a9f046a59adbfd0c3a1dca0f03158b3d6c56463
rajeev <rajeevriitm@gmail.com>
2015-07-25 10:13:07 +0530

    wierds

--- db/migrate/20150723142249_create_activities.rb
+++ db/migrate/20150723142249_create_activities.rb
@@ -4,9 +4,10 @@
       t.references :user, index: true, foreign_key: true
       t.references :action, index: true, polymorphic: true
       t.references :micropost, index: true, foreign_key: true
-      t.references :owner,index:true,foreign_key: true
+      t.integer :owner_id
       t.timestamps null: false
     end
     add_index :activities,:action_id
+    add_index :activities,:owner_id
   end
 end
